87557ecc79c06a4df4648178dbc7100b130e2186
Kajetan.Niewiarowski <98642117+JustALitlle@users.noreply.github.com>
2022-06-17 13:49:09 +0200

    Add files via upload

---
+++ ProjectForCodersLab/src/main/java/pages/myStore/MyStoreLoginPage.java
@@ -0,0 +1,27 @@
+package pages.myStore;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class MyStoreLoginPage {
+    private WebDriver driver;
+
+    public MyStoreLoginPage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+    @FindBy(name = "email")
+    private WebElement loginEmailInput;
+    @FindBy(name = "password")
+    private WebElement passwordInput;
+    @FindBy(id = "submit-login")
+    private WebElement submitLoginButton;
+
+    public void loginToMyStore(String email, String password) {
+        loginEmailInput.sendKeys(email);
+        passwordInput.sendKeys(password);
+        submitLoginButton.click();
+    }
+}
---
+++ ProjectForCodersLab/src/test/java/myStore/StepDefs.java
@@ -0,0 +1,66 @@
+package myStore;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import pages.myStore.CreateNewAddressPage;
+import pages.myStore.FillTheNewAddressFormPage;
+import pages.myStore.MyStoreLoginPage;
+
+import java.time.Duration;
+
+public class StepDefs {
+
+    private WebDriver driver;
+
+    @Given("open browser with test Store")
+    public void openMyStoreLogin() {
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/driver/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get("https://mystore-testlab.coderslab.pl/index.php?controller=authentication&back=addresses");
+
+    }
+
+    @When("login on account asadas@10minutemail.com Waldekn22")
+    public void loginToMyStore() {
+        MyStoreLoginPage myStoreLoginPage = new MyStoreLoginPage(driver);
+        myStoreLoginPage.loginToMyStore("asadas@10minutemail.com", "Waldekn22");
+    }
+
+    @And("create new address")
+    public void createNewAddress() {
+        CreateNewAddressPage createNewAddress = new CreateNewAddressPage(driver);
+        createNewAddress.createNewAddressClick();
+    }
+
+    @When("fill out the form of new address with {word}, {word}, {word}, {word}, {word}")
+    public void fillOutNewAddress(String keyword1, String keyword2, String keyword3, String keyword4, String keyword5) {
+        FillTheNewAddressFormPage fillOutNewAddress = new FillTheNewAddressFormPage(driver);
+        fillOutNewAddress.NewAddressRegister(keyword1, keyword2, keyword3, keyword4, keyword5);
+    }
+
+    @Then("new address should be added")
+    public void addNewAddress() {
+        FillTheNewAddressFormPage fillOutNewAddressButton = new FillTheNewAddressFormPage(driver);
+        fillOutNewAddressButton.AddNewAddress();
+
+        CreateNewAddressPage createNewAddress = new CreateNewAddressPage(driver);
+       Assert.assertEquals("Address successfully added!", createNewAddress.getAddressNotify());
+
+
+
+
+    }
+
+    @And("close the browser")
+    public void closeBrowser(){
+        driver.quit();
+    }
+
+}
